4e04c36e0d62ddbcce2bea1a37c6f3d3a28c8307
AmyMcMahon <admcmahon@live.com[3~[3~>
2024-05-13 12:15:33 +0100

    add password hashing

--- modules/database.py
+++ modules/database.py
@@ -1,10 +1,12 @@
 import sqlite3
+import modules.encryption as enc
 
-class Database():
+
+class Database:
     def __init__(self):
         self.connect = sqlite3.connect("database.db", check_same_thread=False)
         self.connect.execute(
-        "CREATE TABLE IF NOT EXISTS USERS (username TEXT, password TEXT, public_key TEXT)"
+            "CREATE TABLE IF NOT EXISTS USERS (username TEXT, password TEXT, public_key TEXT)"
         )
         self.connect.execute(
             "CREATE TABLE IF NOT EXISTS FILES (sender TEXT, receiver TEXT, file_path TEXT, id INTEGER PRIMARY KEY AUTOINCREMENT)"
@@ -12,6 +14,7 @@ class Database():
         self.cursor = self.connect.cursor()
 
     def createUser(self, username, password, public_key):
+        password = enc.hash_password(password)
         self.cursor.execute(
             "INSERT INTO USERS(username, password, public_key) VALUES (?, ?, ?)",
             (username, password, public_key),
@@ -20,10 +23,12 @@ class Database():
 
     def getPassword(self, username):
 
-        self.cursor.execute('SELECT password FROM USERS WHERE username = "' + username + '"')
+        self.cursor.execute(
+            'SELECT password FROM USERS WHERE username = "' + username + '"'
+        )
         row = self.cursor.fetchone()
         return row[0]
-    
+
     def insertFile(self, sender, receiver, file_path):
 
         self.cursor.execute(
@@ -32,33 +37,34 @@ class Database():
         )
         self.connect.commit()
 
-    #for debug, get rid of ltr
+    # for debug, get rid of ltr
     def getAllUsersAdmin(self):
         self.cursor.execute("SELECT username, password, public_key FROM USERS")
         rows = self.cursor.fetchall()
         return rows
 
-    
     def getAllUsers(self):
         self.cursor.execute("SELECT username FROM USERS")
         rows = self.cursor.fetchall()
         return rows
 
     def getUsersFiles(self, username):
-        self.cursor.execute('SELECT sender, file_path FROM FILES WHERE sender = "' + username + '"')
+        self.cursor.execute(
+            'SELECT sender, file_path FROM FILES WHERE sender = "' + username + '"'
+        )
         rows = self.cursor.fetchall()
         clean_rows = []
         for sender, file_path in rows:
             file_path = file_path.split("/")[-1]
             clean_rows.append((sender, file_path))
         return clean_rows
-    
-    #maybe get rid of this ltr
+
+    # maybe get rid of this ltr
     def getAllFilesAdmin(self):
-        self.cursor.execute('SELECT sender, receiver, file_path FROM FILES')
+        self.cursor.execute("SELECT sender, receiver, file_path FROM FILES")
         rows = self.cursor.fetchall()
         clean_rows = []
         for sender, receiver, file_path in rows:
             file_path = file_path.split("/")[-1]
-            clean_rows.append((sender,receiver, file_path))
+            clean_rows.append((sender, receiver, file_path))
         return clean_rows
