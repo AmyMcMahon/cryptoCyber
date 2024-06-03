e06cde06be4b0e0f12bfcedc579e886fcd6fb020
Derv6464 <dervlag6464@gmail.com>
2024-06-03 14:41:13 +0100

    this might work??

--- selinum_scripts/sender.py
+++ selinum_scripts/sender.py
@@ -1,12 +1,39 @@
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
-from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome.service import Service
+import chromedriver_autoinstaller
+from pyvirtualdisplay import Display
 import time
 import os
+display = Display(visible=0, size=(800, 800))  
+display.start()
 
-options = Options()
-options.headless = True
-driver = webdriver.Chrome(options=options)
+chromedriver_autoinstaller.install()  # Check if the current version of chromedriver exists
+                                      # and if it doesn't exist, download it automatically,
+                                      # then add chromedriver to path
+
+chrome_options = webdriver.ChromeOptions()    
+# Add your options as needed    
+options = [
+  # Define window size here
+   "--window-size=1200,1200",
+    "--ignore-certificate-errors"
+ 
+    #"--headless",
+    #"--disable-gpu",
+    #"--window-size=1920,1200",
+    #"--ignore-certificate-errors",
+    #"--disable-extensions",
+    #"--no-sandbox",
+    #"--disable-dev-shm-usage",
+    #'--remote-debugging-port=9222'
+]
+
+for option in options:
+    chrome_options.add_argument(option)
+
+driver = webdriver.Chrome(options = chrome_options)
+
 driver.get("http://127.0.0.1:8000")
 
 relative_path = './selinum_scripts/testing.txt'
