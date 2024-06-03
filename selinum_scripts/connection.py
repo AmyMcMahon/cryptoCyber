e970638fc79348c501af921e5e926dd0c43c4c4d
Derv6464 <dervlag6464@gmail.com>
2024-06-03 17:05:08 +0100

    why do vm's hate me

--- selinum_scripts/connection.py
+++ selinum_scripts/connection.py
@@ -5,5 +5,8 @@ output = subprocess.check_output(["curl", "http://127.0.0.1:8000/"])
 output_str = output.decode("utf-8")
 
 while "html" not in output_str:
-    output = subprocess.check_output(["curl", "http://127.0.0.1:8000/"])
-    output_str = output.decode("utf-8")
+    try:
+        output = subprocess.check_output(["curl", "http://127.0.0.1:8000/"])
+        output_str = output.decode("utf-8")
+    except:
+        pass
