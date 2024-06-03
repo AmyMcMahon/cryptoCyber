48e80f4993959068ed38cf7207cd7454dbcd0a51
Derv6464 <dervlag6464@gmail.com>
2024-06-03 17:22:06 +0100

    base case

--- selinum_scripts/connection.py
+++ selinum_scripts/connection.py
@@ -1,8 +1,10 @@
 import subprocess
 
-
-output = subprocess.check_output(["curl", "http://127.0.0.1:8000/"])
-output_str = output.decode("utf-8")
+try:
+    output = subprocess.check_output(["curl", "http://127.0.0.1:8000/"])
+    output_str = output.decode("utf-8")
+except:
+    output_str = ""
 
 while "html" not in output_str:
     try:
