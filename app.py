65744cd5debe9ab1c256c8fbca1fd789ade5a182
Derv6464 <dervlag6464@gmail.com>
2024-06-04 09:21:50 +0100

    updating file paths

--- app.py
+++ app.py
@@ -54,7 +54,7 @@ def index():
         if db.check_Login(username, password):
             userToLogin = db.getUser(username)
             login_user(userToLogin, remember=False)
-            return redirect(url_for("user"))
+            return redirect(url_for("script/user"))
         else:
             return jsonify(error="Invalid username or password"), 401
     return render_template("index.html")
@@ -64,7 +64,7 @@ def index():
 @login_required
 def logout():
     logout_user()
-    return redirect(url_for("script/index"))
+    return redirect(url_for("index"))
 
 
 # Route for create account page
